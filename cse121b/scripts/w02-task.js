9f62cf7e2e2908fc03ff5dca1f8887be701d1061
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2023-11-13 16:29:49 -0500

    Add files via upload

--- cse121b/scripts/w02-task.js
+++ cse121b/scripts/w02-task.js
@@ -1,42 +1,43 @@
-/* W02-Task - Profile Home Page */
+    /* W02-Task - Profile Home Page */
 
-/* Step 1 - Setup type tasks - no code required */
+    /* Step 1 - Setup type tasks - no code required */
 
-/* Step 2 - Variables */
+    /* Step 2 - Variables */
 
-let fullName = 'Alexandre Espinoza';
-let currentYear = new Date().getFullYear();
-let profilePicture = 'images/mypicture.jpg';
+    const fullName = 'Alexandre Espinoza';
+    const currenteDate = new Date();
+    const currentYear = currenteDate.getFullYear();
+    const profilePicture = 'images/mypicture.jpg';
 
-/* Step 3 - Element Variables */
+    /* Step 3 - Element Variables */
 
-const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('year');
-const imageElement = document.querySelector('image');
+    const nameElement = document.getElementById ("name");
+    const foodElement = document.getElementById ("food");
+    const yearElement = document.querySelector ("#year");
+    const imageElement = document.querySelector ("img");
 
-/* Step 4 - Adding Content */
+    /* Step 4 - Adding Content */
 
-nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.textContent = currentYear;
-imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+    nameElement.innerHTML = `<strong>${fullName}</strong>`;
+    yearElement.textContent = currentYear;
+    imageElement.src = profilePicture;
+    imageElement.alt = `Profile image of ${fullName}`;
 
-/* Step 5 - Array */
+    /* Step 5 - Array */
 
-const favoriteFoods = ["Chips","Burgers","Pizza","Encebollado","Bolon"];
-foodElement.innerHTML = favoriteFoods.join(",");
+    const favoriteFoods = ["Chips","Burgers","Pizza","Encebollado","Bolon"];
+    foodElement.textContent = favoriteFoods.join(",");
 
-const newFavoriteFood = "Chocolate Ice Cream";
-favoriteFoods.push(newFavoriteFood);
+    let singleFavoriteFood = "Chocolate Ice Cream";
+    favoriteFoods.push(singleFavoriteFood);
 
-foodElement.innerHTML += `<br>${favoriteFoods.join(",")}`;
+    foodElement.innerHTML += `<br>${favoriteFoods.join(",")}`;
 
-favoriteFoods.shift();
-foodElement.innerHTML += `<br>${favoriteFoods.join(",")}`;
+    favoriteFoods.shift();
+    foodElement.innerHTML += `<br>${favoriteFoods.join(",")}`;
 
-favoriteFoods.pop();   
-foodElement.innerHTML += `<br>${favoriteFoods.join(",")}`;
+    favoriteFoods.pop();   
+    foodElement.innerHTML += `<br>${favoriteFoods.join(",")}`;
 
 
 
