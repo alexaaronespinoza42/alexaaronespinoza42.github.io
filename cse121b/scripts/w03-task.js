c12b1f334d2121a4e56f828a92921791779e6f7b
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2023-11-19 21:59:47 -0500

    CORRECTIONS!

--- cse121b/scripts/w03-task.js
+++ cse121b/scripts/w03-task.js
@@ -11,13 +11,13 @@ function addNumbers() {
     document.querySelector("#sum").value = add(addNumber1, addNumber2);
 }
 /* Function Expression - Subtract Numbers */
-function substract (number1, number2){
+function subtract (number1, number2){
     return number1 - number2;
 }
-function substractNumbers() {
-    let substractNumber1 = Number(document.querySelector("#substract1").value);
-    let substractNumber2 = Number(document.querySelector("#substract2").value);
-    document.querySelector("#difference").value = substract(substractNumber1, substractNumber2);
+function subtractNumbers() {
+    let subtractNumber1 = Number(document.querySelector("#subtract1").value);
+    let subtractNumber2 = Number(document.querySelector("#subtract2").value);
+    document.querySelector("#difference").value = subtract(subtractNumber1, subtractNumber2);
 }
 /* Arrow Function - Multiply Numbers */
 const multiply = (number1, number2) => number1 * number2;
@@ -37,36 +37,46 @@ const divideNumbers = () => {
     document.querySelector("#quotient").value = divide(divideNumber1, divideNumber2);
 }
 
+/* Missing */
+document.querySelector("#addNumbers").addEventListener("click", addNumbers);
+document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers);
+document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
+document.querySelector("#divideNumbers").addEventListener("click", divideNumbers);
+
 /* Decision Structure */
-document.querySelector("#getTotalDue").addEventListener("click", calculateTotal);
+                        /* getTotal instead of getTotalDue */
+document.querySelector("#getTotal").addEventListener("click", calculateTotal);
 
 function calculateTotal(){
+    /* member instead of membership */
     let subTotal = Number(document.querySelector("#subtotal").value);
-    let applyDiscount = document.querySelector("#membership").checked;
+    let applyDiscount = document.querySelector("#member").checked;
 
-    if (applyDiscount){
-        subtotal *=0.85;
+    if (applyDiscount) {
+        subTotal *= 0.85;
     }
-    document.querySelector("#total").textContent = "Total due: $${subtotal.toFixed(2)}";
+    /* ` instead of " " */
+    document.querySelector("#total").textContent = `Total due: $${subTotal.toFixed(2)}`;
 }
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
 let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-document.querySelector("#array").textContent = numbersArray;S
+document.querySelector("#array").textContent = numbersArray;
 
 /* Output Odds Only Array */
 let oddNumbers = numbersArray.filter(number => number % 2 === 1);
 document.querySelector("#odds").textContent = oddNumbers;
+
 /* Output Evens Only Array */
 document.querySelector("#evens").innerHTML = numbersArray.filter(number => number % 2 === 0);
 
 /* Output Sum of Org. Array */
 let sumOfArray = numbersArray.reduce((sum, number) => sum + number, 0);
-document.querySelector("#sumOfArray").textContent = sumOfArray;}
+document.querySelector("#sumOfArray").textContent = sumOfArray;
 
 /* Output Multiplied by 2 Array */
-let multiplied = numbersArray.map(number => number*2);
+let multiplied = numbersArray.map(number => number * 2);
 document.querySelector("#multiplied").textContent = multiplied;
 
 /* Output Sum of Multiplied by 2 Array */
