0c509cb5e3ecfd3204803c768e03ae3cbd4c3327
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2023-12-09 16:02:22 -0500

    NEW

--- cse121final/gymReviews.js
+++ cse121final/gymReviews.js
@@ -1,24 +1,19 @@
-// Llamada a fetch para obtener las revisiones
 const getReviews = async () => {
     try {
       const response = await fetch("https://alexaaronespinoza42.github.io/cse121final/resources/reviews.json");
       const data = await response.json();
       
-      // Llamar a la función para mostrar las revisiones
       displayReviews(data);
     } catch (error) {
       console.error("Error fetching reviews:", error);
     }
   };
   
-  // Función para mostrar las revisiones en el HTML
   const displayReviews = (reviews) => {
-    const reviewsContainer = document.querySelector('.reviews-container');
+    const reviewsContainer = document.querySelector('.reviews__container');
   
-    // Limpiar contenido existente
     reviewsContainer.innerHTML = "";
   
-    // Iterar sobre las revisiones y agregarlas al HTML
     reviews.forEach(review => {
       const reviewElement = document.createElement('div');
       reviewElement.classList.add('review');
@@ -31,5 +26,4 @@ const getReviews = async () => {
     });
   };
   
-  // Llamar a la función para obtener y mostrar revisiones al cargar la página
   getReviews();
