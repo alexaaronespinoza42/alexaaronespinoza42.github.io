573e2ce17c244043bd0206eed2e4f55bd5d5d832
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2024-03-02 21:18:31 -0500

    asd

--- wdd230notofficial/scripts/hamburger.js
+++
@@ -1,6 +0,0 @@
-const hamButton = document.querySelector('#hamburger');
-const navMenu = document.querySelector('nav ul');
-
-hamButton.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-});
